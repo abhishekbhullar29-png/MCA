7c6d34b7c78862cc2a41d97f8d0683a04750b7d5
abhishekbhullar29-png <abhishekbhullar29@gmail.com>
2026-04-14 00:49:43 +0530

    Add files via upload

---
+++ frontend-new - Copy/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+
+  server: {
+    port: 5173,
+
+    proxy: {
+      '/employees': {   // ✅ same as backend API
+        target: 'http://localhost:8080', // ✅ Spring Boot port
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
+})
